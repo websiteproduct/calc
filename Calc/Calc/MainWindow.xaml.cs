b6eeb2e051ab42820ff830283cb4e772c47a3354
PavelEagle <47183588+PavelEagle@users.noreply.github.com>
2020-01-08 04:23:15 +0300

    Add methods for buttons
    - add method for 0-9 button
    - add methods for clear, del and sign change button

--- Calc/Calc/MainWindow.xaml.cs
+++ Calc/Calc/MainWindow.xaml.cs
@@ -14,17 +14,31 @@ namespace Calc
             InitializeComponent();
         }
 
-        private void Button_7_Click(object sender, RoutedEventArgs e)
+        private void Button_Click(object content)
         {
             if (test.Text == "0")
             {
-                test.Text = Button_7.Content.ToString();
+                test.Text = content.ToString();
             }
             else
             {
-                test.Text += Button_7.Content.ToString();
+                test.Text += content.ToString();
             }
         }
+
+        private void Button_7_Click(object sender, RoutedEventArgs e)
+        {
+            Button_Click(Button_7.Content);
+            //if (test.Text == "0")
+            //{
+            //    test.Text = Button_7.Content.ToString();
+            //}
+            //else
+            //{
+            //    test.Text += Button_7.Content.ToString();
+            //}
+        }
+
 
         private void Window_KeyUp(object sender, KeyEventArgs e)
         {
@@ -32,6 +46,80 @@ namespace Calc
             {
                 Button_7_Click(sender, e);
             }
+        }
+
+        private void Button_0_Click(object sender, RoutedEventArgs e)
+        {
+            Button_Click(Button_0.Content);
+        }
+
+        private void Button_1_Click(object sender, RoutedEventArgs e)
+        {
+            Button_Click(Button_1.Content);
+        }
+
+        private void Button_2_Click(object sender, RoutedEventArgs e)
+        {
+            Button_Click(Button_2.Content);
+        }
+
+        private void Button_3_Click(object sender, RoutedEventArgs e)
+        {
+            Button_Click(Button_3.Content);
+        }
+
+        private void Button_4_Click(object sender, RoutedEventArgs e)
+        {
+            Button_Click(Button_4.Content);
+        }
+        private void Button_5_Click(object sender, RoutedEventArgs e)
+        {
+            Button_Click(Button_5.Content);
+        }
+
+        private void Button_6_Click(object sender, RoutedEventArgs e)
+        {
+            Button_Click(Button_6.Content);
+        }
+
+        private void Button_8_Click(object sender, RoutedEventArgs e)
+        {
+            Button_Click(Button_8.Content);
+        }
+
+        private void Button_9_Click(object sender, RoutedEventArgs e)
+        {
+            Button_Click(Button_9.Content);
+        }
+
+        private void Button_Del_Click(object sender, RoutedEventArgs e)
+        {
+            if (test.Text.Length > 1)
+            {
+                test.Text = test.Text.Substring(0, test.Text.Length - 1);
+            }
+            else
+            {
+                test.Text = "0";
+            }
+        }
+
+        private void Button_signChange_Click(object sender, RoutedEventArgs e)
+        {
+            if (Convert.ToInt32(test.Text) > 0)
+            {
+                test.Text = "-" + test.Text;
+            }
+            else
+            {
+                test.Text = test.Text.Substring(1, test.Text.Length-1);
+            }
+            
+        }
+
+        private void Button_Clear_Click(object sender, RoutedEventArgs e)
+        {
+            test.Text = "0";
         }
     }
 }
